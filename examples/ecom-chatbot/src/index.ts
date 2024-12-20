2da2229fe0a244628af8e20a35fa3a4b5c06f53d
Michael Brecht <brecht.michael.b@gmail.com>
2024-12-20 16:14:41 -0700

    chore: update model version

--- examples/ecom-chatbot/src/index.ts
+++ examples/ecom-chatbot/src/index.ts
@@ -3,15 +3,18 @@ import "dotenv/config";
 
 const model = new ChatOpenAI({
   openAIApiKey: process.env.OPENAI_API_KEY,
-  modelName: "gpt-4",
+  modelName: "gpt-4o",
   temperature: 0.3,
   maxTokens: 1000,
 });
 
 (async () => {
-  const prompt = "Tell me the name of the current US President";
+  const prompts = [
+    "What is the name on the US President",
+    "Who was the President in 2018",
+  ];
 
-  const response = await model.invoke(prompt);
+  const response = await model.batch(prompts);
 
   console.log(response);
 })();
